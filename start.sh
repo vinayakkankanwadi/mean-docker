4c79896fdea688465a59a71bacfbce43dca9bc9d
Vinayak Kankanwadi <vinayakkankanwadi@hotmail.com>
2016-06-14 01:08:34 +1000

    App run

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ cd /tmp
 # try to remove the repo if it already exists
 rm -rf mean-docker; true
 
-git clone https://github.com/vinayakkankanwadi/mean-docker.git
+git clone https://github.com/vinayakkankanwadi/mean-docker.git mean-docker
 
 cd mean-docker
 
